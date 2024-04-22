e8b36fb8e851fea26c99c9baa6434ad5a6a38ed2
arvndblocker"nn <aravindvasang@gmail.com>
2024-04-22 11:40:56 +0530

    mint

--- src/CLAMM.sol
+++ src/CLAMM.sol
@@ -3,9 +3,14 @@ pragma solidity ^0.8.19;
 
 import {Tick} from "./lib/Tick.sol";
 import {TickMath} from "./lib/TickMath.sol";
+import {Position} from "./lib/Position.sol";
+import {SafeCast} from "./lib/SafeCast.sol";
+import {IERC20} from "./interface/IERC20.sol";
 
 contract Clamm {
     /////////////////state variables//////////////
+    using SafeCast for int256;
+
     address public immutable token0;
     address public immutable token1;
     uint24 public immutable fee;
@@ -19,6 +24,22 @@ contract Clamm {
     }
 
     Slot0 public slot0;
+    mapping(bytes32 => Position.Info) positions;
+
+    struct ModifyPositionParams {
+        address owner;
+        int24 tickLower;
+        int24 tickUpper;
+        int128 liquidityDelta;
+    }
+
+    ///////////////////modifier/////////////////
+    modifier lock() {
+        require(slot0.unlocked, "locked");
+        slot0.unlocked = false;
+        _;
+        slot0.unlocked = true;
+    }
 
     constructor(address _token0, address _token1, uint24 _fee, int24 _tickSpacing) {
         token0 = _token0;
@@ -35,5 +56,38 @@ contract Clamm {
         int24 tick = TickMath.getTickAtSqrtRatio(sqrtPriceX96);
 
         slot0 = Slot0({sqrtPriceX96: sqrtPriceX96, tick: tick, unlocked: true});
+    }
+
+    function _modifyPosition(ModifyPositionParams memory params)
+        private
+        returns (Position.Info storage position, int256 amount0, int256 amount1)
+    {
+        return (positions[bytes32(0)], 0, 0);
+    }
+
+    function mint(address recipient, int24 tickLower, int24 tickUpper, uint128 amount)
+        external
+        lock
+        returns (uint256 amount0, uint256 amount1)
+    {
+        require(amount > 0);
+        (, int256 amount0Int, int256 amount1Int) = _modifyPosition(
+            ModifyPositionParams({
+                owner: recipient,
+                tickLower: tickLower,
+                tickUpper: tickUpper,
+                liquidityDelta: int256(uint256(amount)).toInt128()
+            })
+        );
+        amount0 = uint256(amount0Int);
+        amount1 = uint256(amount1Int);
+
+        if (amount0 > 0) {
+            IERC20(token0).transferFrom(msg.sender, address(this), amount0);
+        }
+
+        if (amount1 > 0) {
+            IERC20(token1).transferFrom(msg.sender, address(this), amount0);
+        }
     }
 }
